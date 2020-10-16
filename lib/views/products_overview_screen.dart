212cfac80670134f8f3d12b31765e378948a9e7a
Guilherme Aguiar <kadeguilherme@gmail.com>
2020-10-16 09:34:51 -0300

    Criando na pasta model o arquivo product_item e tambem criamos o GridTitleBar

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shop/models/product.dart';
 import '../data/dummy_data.dart';
 import '../models/product.dart';
+import '../widgets/product_item.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
   final List<Product> loadedProducts = DUMMY_PRODUCTS;
@@ -15,9 +16,11 @@ class ProductOverviewScreen extends StatelessWidget {
       body: GridView.builder(
         padding: const EdgeInsets.all(10),
         itemCount: loadedProducts.length,
-        itemBuilder: (ctx, i) => Text(loadedProducts[i].title),
+        itemBuilder: (ctx, i) => ProductItem(loadedProducts[i]),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          //quantidade de item na tela
           crossAxisCount: 2,
+          //tamanho de cada item na tela
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 10,
           mainAxisSpacing: 10,
