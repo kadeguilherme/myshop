36f4aecb82423bfbda2796add1c06dbd61fdd786
Guilherme Aguiar <kadeguilherme@gmail.com>
2020-11-03 09:45:56 -0300

    Usando o Provider para mudar o estado de aplicação dos favorito na aplicação toda

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/widgets/product_grid.dart';
+import '../providers/products.dart';
+
+enum FilterOptions {
+  Favorite,
+  All,
+}
 
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Products products = Provider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text("Minha loja"),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.Favorite) {
+                products.showFavoriteOnly();
+              } else {
+                products.showAll();
+              }
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Somente Favoritos'),
+                value: FilterOptions.Favorite,
+              ),
+              PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOptions.All,
+              ),
+            ],
+          ),
+        ],
       ),
       body: ProductGrid(),
     );
