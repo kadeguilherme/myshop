dcdbd909c386517face40faf984ef6228dd8a162
Guilherme Aguiar <kadeguilherme@gmail.com>
2020-11-05 10:28:23 -0300

    Construindo parte que mostra na tela os items no cart_screen

--- lib/views/cart_screen.dart
+++ lib/views/cart_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/cart.dart';
+import '../widgets/cart_item_widget.dart';
 
 class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Cart cart = Provider.of(context);
+    final cartItem = cart.item.values.toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Carrinho'),
@@ -41,6 +44,13 @@ class CartScreen extends StatelessWidget {
                   )
                 ],
               ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.intemCount,
+              itemBuilder: (ctx, i) => CartItemWidget(cartItem[i]),
             ),
           )
         ],
