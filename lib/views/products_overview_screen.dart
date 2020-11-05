a570d17f3cbdf3278f978b6d5282247077b68782
Guilherme Aguiar <kadeguilherme@gmail.com>
2020-11-05 10:01:28 -0300

    Construindo widget da tela cart_screen

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/providers/cart.dart';
+import 'package:shop/utils/app_routes.dart';
 import 'package:shop/widgets/badge.dart';
 import 'package:shop/widgets/product_grid.dart';
 
@@ -51,7 +52,9 @@ class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
               valor: cart.intemCount.toString(),
               child: IconButton(
                 icon: Icon(Icons.shopping_cart),
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.of(context).pushNamed(AppRoutes.CART);
+                },
               ),
             ),
           ),
